03b733ae548b717db0d4181d662bdcd1c8004796
Stan <dimitrov.s.dev@gmail.com>
2023-09-15 09:01:09 +0300

    Snowflake_MasterClass

---
+++ 2.Copy Options/4.Return_Failed_Only.sql
@@ -0,0 +1,50 @@
+---- RETURN_FAILED_ONLY ----
+
+CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+// Prepare stage object
+CREATE OR REPLACE STAGE COPY_DB.PUBLIC.aws_stage_copy
+    url='s3://snowflakebucket-copyoption/returnfailed/';
+
+LIST @COPY_DB.PUBLIC.aws_stage_copy;
+
+
+ //Load data using copy command
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    FILE_FORMAT = (type = csv field_delimiter=',' skip_header=1)
+    PATTERN = '.*Order.*'
+    RETURN_FAILED_ONLY = TRUE;
+
+
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    FILE_FORMAT = (type = csv field_delimiter=',' skip_header=1)
+    PATTERN ='.*Order.*'
+    ON_ERROR =CONTINUE
+    RETURN_FAILED_ONLY = TRUE;
+
+
+// Default = FALSE
+
+CREATE OR REPLACE TABLE  COPY_DB.PUBLIC.ORDERS (
+    ORDER_ID VARCHAR(30),
+    AMOUNT VARCHAR(30),
+    PROFIT INT,
+    QUANTITY INT,
+    CATEGORY VARCHAR(30),
+    SUBCATEGORY VARCHAR(30));
+
+
+COPY INTO COPY_DB.PUBLIC.ORDERS
+    FROM @aws_stage_copy
+    FILE_FORMAT = (type = csv field_delimiter=',' skip_header=1)
+    PATTERN = '.*Order.*'
+    ON_ERROR =CONTINUE;
